9de30ad66bdb27f571e2aafe5afe3bd9e75248ee
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-25 17:00:59 -0300

    Implementado o requisito 01

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    // const { title } = this.props;
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
+
+// {data.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
